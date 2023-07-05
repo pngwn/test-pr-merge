4133bcd714da4a38168715716f9d83a1d6b8461b
pngwn <hello@pngwn.io>
2023-07-05 13:27:39 +0100

    tests

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export default function util() {
   return true;
 }
+
+export function util_two() {
+  return false;
+}
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,12 @@
 import { test, expect } from 'vitest';
-import util from './index.js'
+import util, { util_two } from './index.js'
 
 test('should be true', () => {
   const val = util();
   expect(val).toBe(true);
+});
+
+test('should be false', () => {
+  const val = util_two();
+  expect(val).toBe(false);
 });
